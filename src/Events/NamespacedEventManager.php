0ed772bd2f9154bc3380745b81b6ab7fe401399a
Martin Lutonsky <martin.lutonsky@inspire.cz>
2026-04-22 22:00:37 +0200

    allow array properties to be bound to EventManager by wrapping events to array
    (cherry picked from commit dee1b7eb8b8252cedb192aed2fcce6b7b116d0b9)

--- src/Events/NamespacedEventManager.php
+++ src/Events/NamespacedEventManager.php
@@ -145,7 +145,7 @@ class NamespacedEventManager extends \Kdyby\Events\EventManager
 	/**
 	 * {@inheritDoc}
 	 */
-	public function createEvent($name, $defaults = [], $argsClass = NULL, $globalDispatchFirst = FALSE)
+	public function createEvent($name, $defaults = [], $argsClass = NULL, $globalDispatchFirst = FALSE): Event
 	{
 		return $this->evm->createEvent($this->namespace . $name, $defaults, $argsClass, $globalDispatchFirst);
 	}
